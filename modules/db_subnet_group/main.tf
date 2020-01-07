1c641a33b3365b84260414457a5cb900f53a399b
Dinesh Auti <dinesh.auti@aetion.com>
2020-01-07 17:44:22 -0500

    if subnet_group_name is empty use a default one

--- modules/db_subnet_group/main.tf
+++ modules/db_subnet_group/main.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "this" {
   count = var.create ? 1 : 0
 
-  name = var.subnet_group_name
+  name = var.subnet_group_name == "" ? "${var.identifier}-subnetgrp" : var.subnet_group_name
   description = "Database subnet group for ${var.identifier}"
   subnet_ids  = var.subnet_ids
 
